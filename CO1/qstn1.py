a867de9a76cad140a58c7f4118bd5cab08fb2ed2
Athiram24 <2242@tkmce.ac.in>
2023-09-19 06:42:36 +0530

    Added co1

---
+++ CO1/qstn1.py
@@ -0,0 +1,12 @@
+# to find mean,median,and standard deviation
+import numpy as np
+arr = np.array([76,81,82,84,84,85,97])
+meanvalue = np.mean(arr)
+medianvalue = np.median(arr)
+sd = np.std(arr)
+print("Mean = ",meanvalue)
+print("Median = ",medianvalue)
+print("standard deviation = ",sd)
+
+
+
